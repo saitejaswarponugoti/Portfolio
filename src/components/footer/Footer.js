12c8718c2186db7693f669f7a9caa62c86a30d72
Sai Tejaswar Ponugoti <saitejaswar.ponugoti@gmail.com>
2024-12-08 14:44:30 -0600

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,7 +10,7 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Revamped with ❤️ by Sai Tejaswar, inspired by {" "}
+          Revamped by Sai Tejaswar, inspired by {" "}
           <a
             href="https://github.com/saadpasta/developerFolio"
             target="_blank"
